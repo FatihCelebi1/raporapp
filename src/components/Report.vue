e1dae90dd8cf41aab5800238bbbae3b62fc499ab
Fatih Çelebi <celebigg02@gmail.com>
2022-09-11 22:45:56 +0300

    raporlar veritabanına bağlandı

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -4,7 +4,6 @@
       <v-row no-gutters>
         <v-col cols="12" sm="6" md="10">
           <h3>Raporlar</h3>
-          {{allReports}}
           <p class="grey--text text-darken-1 mb-2">
             Bu sayfa üzerinden günlük harcamalarınızı girebilirsiniz, Kalan
             miktar hesaplaması yapabilmeniz için Profilinizden aylık gelir
@@ -12,65 +11,77 @@
           </p>
         </v-col>
         <v-col cols="3" md="2">
-          <v-btn color="#038C3E" elevation="2" outlined>Ödeme Ekle<v-icon class="ml-2">mdi-table-row-plus-after</v-icon></v-btn>
+          <v-btn color="#038C3E" elevation="2" outlined
+            >Ödeme Ekle<v-icon class="ml-2"
+              >mdi-table-row-plus-after</v-icon
+            ></v-btn
+          >
         </v-col>
       </v-row>
       <template>
         <v-data-table
           :headers="headers"
-          :items="desserts"
+          :items="getRecords"
           :items-per-page="-1"
           hide-default-footer
           class="elevation-0 mt-5"
         ></v-data-table>
-         <v-btn color="#038C3E" elevation="2" class="mt-5" outlined>Ödeme Ekle<v-icon class="ml-2">mdi-table-row-plus-after</v-icon></v-btn>
+        <v-btn color="#038C3E" elevation="2" class="mt-5" outlined
+          >Ödeme Ekle<v-icon class="ml-2"
+            >mdi-table-row-plus-after</v-icon
+          ></v-btn
+        >
       </template>
     </v-container>
   </div>
 </template>
 
 <script>
+// const axios = require("axios");
 export default {
   data() {
     return {
+      records: [],
       headers: [
         {
-          text: "Madde",
+          text: "Sıra",
           align: "start",
           sortable: false,
-          value: "name",
+          value: "madde",
         },
         { text: "Harcanan Ödeme", value: "odeme" },
         { text: "Hesap", value: "hesap" },
         { text: "Tarih", value: "tarih" },
       ],
-      desserts: [
-        {
-          name: "Market",
-          odeme: "150₺",
-          hesap: "Enpara",
-          tarih: "20.09.22",
-        },
-        {
-          name: "Benzin",
-          odeme: "350₺",
-          hesap: "Yapıkredi",
-          tarih: "20.09.22",
-        },
-        {
-          name: "Harcama",
-          odeme: "20₺",
-          hesap: "Nakit",
-          tarih: "20.09.22",
-        },
-      ],
     };
   },
-   computed: {
-    allReports() {
-      return this.$store.getters["reports/getReport"];
-    }}
-    
+
+  computed: {
+    getRecords() {
+      console.log('admın 5')
+      return this.$store.getters['reports/getReport'] 
+    }
+  },
+
+  // methods: {
+  // async getRecords() {
+  //   try {
+  // const { data } = await axios.get(
+  //   "http://localhost:8083/raporappv2/rapor-app/includes/reports.php"
+  // );
+
+  // this.records = data.rows;
+
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   },
+  // },
+  async mounted() {
+    // commit => mutations
+    // dispatch => actions
+    console.log('adım 1')
+    await this.$store.dispatch('reports/fetchAllReports', 'ne istersem o');
+  },
 };
-console.log('naber');
 </script>
--- src/store/reports.js
+++ src/store/reports.js
@@ -1,36 +1,36 @@
-const axios = require('axios');
+const axios = require("axios");
 const reports = {
-    namespaced: true,
-    state: {
-        report: [],
-    },
-    mutations: {
-        SET_REPORTS(state, payload) {
-            state.report = payload
-        },
-    },
-    getters: {
-        getReport: (state) => {
-            return state.report;
-          },
-    },
+  namespaced: true,
+  state: {
+    report: [],
+  },
 
-    actions: {
-        getReports({ commit }) {
-          axios({
-            url: "http://localhost:8083/raporappv2/rapor-app/includes/reports.php",
-            method: "get",
-          })
-            .then((res) => {
-           
-              commit("SET_REPORTS", res.data.rows)
-              console.log(res);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-           
-        },
-      },
-  };
-  export default reports;
+  mutations: {
+    SET_REPORTS(state, report) {
+      console.log("adım 3");
+      state.report = report;
+    },
+  },
+
+  getters: {
+    getReport(state) {
+      console.log("adım 4");
+      return state.report;
+    },
+  },
+
+  actions: {
+    async fetchAllReports({ commit }, data) {
+      console.log("data", data);
+      try {
+        const { data } = await axios.get(
+          "http://localhost:8083/raporappv2/rapor-app/includes/reports.php"
+        );
+        commit("SET_REPORTS", data.rows);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+export default reports;
